052de1d49be12d2c2c090299669a3d6cac79d1fb
RMo <morici23@gmail.com>
2022-03-24 22:33:24 -0400

    remove unused services

--- src/services/atlasAPIService.js
+++ src/services/atlasAPIService.js
@@ -1,18 +1,11 @@
 const CLIENT_ID = 'ORrNK32UuC'
 const BASE_URL = `https://api.boardgameatlas.com/api`
 
-const searchGames = (formData) => {
-  console.log(formData)
-  return fetch(`${BASE_URL}/search?name=${formData.query}&client_id=${CLIENT_ID}`)
-  .then(res => res.json())
-}
-
 const searchGameByName = (name) => {
   return fetch(`${BASE_URL}/search?name=${name}&client_id=${CLIENT_ID}`)
   .then(res => res.json())
 }
 
 export {
-  searchGames,
   searchGameByName,
 }
