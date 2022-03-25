a768426ae6503defb0205e24d4b5b84ba3c24b0f
RMo <morici23@gmail.com>
2022-03-24 20:30:22 -0400

    Query api if no search results are returned from our database

--- src/pages/GameSearch/GameSearch.jsx
+++ src/pages/GameSearch/GameSearch.jsx
@@ -1,21 +1,30 @@
 /*-- Helpers --*/
 import { useState } from "react";
+import * as apiServices from '../../services/atlasAPIService'
 
 /*-- Components --*/
 import GameSearchForm from '../../components/GameSearchForm/GameSearchForm'
-
 const GameSearch = ({allGames}) => {
   const [searchResults, setSearchResults] = useState([])
   
   const handleGameSearch = formData => {
     const regex =  new RegExp(formData.name,'gi')
-    let matchingGames = allGames.filter(game => 
+    let databaseMatches = allGames.filter(game => 
     (
       game.name.search(regex) > -1) ? true : false
     )
-    console.log('matching games', matchingGames)
-    // if matching games are empty hit the api
+    console.log(databaseMatches.length, `${formData.name} matches returned from our database`)
+    console.log('matches:', databaseMatches)
     
+    if(databaseMatches.length === 0) {
+      // hit the api
+      apiServices.searchGameByName(formData.name)
+      .then(apiMatches => {
+        console.log(apiMatches?.games.length, `${formData.name} matches returned from api query`)
+        console.log('matches:', apiMatches?.games)
+      })
+    }
+
   }
   
   return (
--- src/services/atlasAPIService.js
+++ src/services/atlasAPIService.js
@@ -1,15 +1,14 @@
-const CLIENT_ID = `${process.env.CLIENT_ID}`
+const CLIENT_ID = 'ORrNK32UuC'
 const BASE_URL = `https://api.boardgameatlas.com/api`
 
 const searchGames = (formData) => {
   console.log(formData)
-  return fetch(`${BASE_URL}/search?&name=${formData.query}&client_id=${CLIENT_ID}`)
+  return fetch(`${BASE_URL}/search?name=${formData.query}&client_id=${CLIENT_ID}`)
   .then(res => res.json())
 }
 
 const searchGameByName = (name) => {
-  console.log(name)
-  return fetch(`${BASE_URL}/search?&name=${name}&client_id=${CLIENT_ID}`)
+  return fetch(`${BASE_URL}/search?name=${name}&client_id=${CLIENT_ID}`)
   .then(res => res.json())
 }
 
