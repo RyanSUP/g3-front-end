f33f2ebce915cf0f0de465c14eb365356e439654
RMo <morici23@gmail.com>
2022-03-25 19:55:02 -0400

    fix bug that causes crash when reloading profile page.

--- src/components/GameList/GameList.jsx
+++ src/components/GameList/GameList.jsx
@@ -4,7 +4,7 @@ const GameList = ({games, user}) => {
 
   return (  
 <div className="row row-cols-1 row-cols-md-2 g-4">
-        {games.map((result, idx) => 
+        {games?.map((result, idx) => 
         <GameCard key={idx} user={user} game={result}/>)}
   </div>
   );
