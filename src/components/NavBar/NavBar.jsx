721cf965cd7676e38b7c33f05f1a55cd244e2c78
Abygaelle <abygaelleb02@gmail.com>
2022-03-25 11:07:11 -0400

    create navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,26 +2,37 @@ import { Link } from 'react-router-dom'
 
 const NavBar = ({ user, handleLogout }) => {
   return (
-    <>
-      {user ?
-        <nav>
-          <ul>
-            <li>Welcome, {user.name}</li>
-            <li><Link to="/profiles">Profiles</Link></li>
-            <li><Link to="/myProfiles">My profile</Link></li>
-            <li><Link to="/games">🔍</Link></li>
-            <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
-          </ul>
+    <>  
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+              <a className="navbar-brand">G3</a>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <Link className="nav-link active" aria-current="page" to="/profiles">Profiles</Link>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <a className="nav-link dropdown-toggle" id="navbarDropdown"  role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Account
+                    </a>
+                    <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <li className="dropdown-item"><Link to="/myProfiles">My Profile</Link></li>
+                      <li className="dropdown-item"><Link to="/changePassword">Change Password</Link></li>
+                      <li><hr className="dropdown-divider"/></li>
+                      <li className="dropdown-item"><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
+                    </ul>
+                  </li>
+                </ul>
+                <form className="d-flex">
+                  <input className="form-control me-2" type="search" placeholder="Find Games" aria-label="Search"/>
+                  <button className="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
         </nav>
-      :
-        <nav>
-          <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
-          </ul>
-        </nav>
-      }
     </>
   )
 }
