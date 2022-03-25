e0d1cfceb78471061bed76853092f9d68098850e
RMo <morici23@gmail.com>
2022-03-25 15:09:17 -0400

    Populate profile games

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,10 +1,12 @@
+import GameList from "../../components/GameList/GameList";
+
 const Profile = ({profile}) => {
 
   return (
     <>
       {profile.name}
       {/* Get all of the profiles games */}
-
+      <GameList games={profile.games} />
     </>
   );
 }
