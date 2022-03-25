a1bc31bb3eb443f46cf2b1bdbfca2c8dc7c14a52
Erin ODea <odea.erin@gmail.com>
2022-03-25 10:46:22 -0500

    card view for games in search

---
+++ src/components/GameCard/GameCard.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const GameCard = ({game}) => {
+
+  return (  
+<div className="card" style={{width: '18rem'}}>
+  <img src={game.thumb_url} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{game.name}</h5>
+    <p className="card-text">description goes here</p>
+    <button className="btn btn-primary mx-2">Details</button>
+    <button  className="btn btn-primary mx-2">Add Game</button>
+  </div>
+</div>
+  );
+}
+
+export default GameCard;
