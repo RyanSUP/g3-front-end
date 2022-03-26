8067a0374b7ab92353970c5a235bd0858015b3d5
RMo <morici23@gmail.com>
2022-03-26 13:40:48 -0400

    add delete buttons swap depending on owner status

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,16 +1,29 @@
+import { useState, useEffect } from 'react'
 import * as profileService from '../../services/profileService'
 const GameCard = ({game, profile, addGameToState}) => {
+  const [buttonType, setButtonType] = useState()
+
+  useEffect(()=> {
+    if(profile.games?.find(profileGame => profileGame._id === game._id)) {
+      setButtonType('delete')
+    } else {
+      setButtonType('add')
+    }
+  }, [profile])
 
   const handleAddGame = () => {
     // add game to profile
     profileService.addGame(profile._id, game)
     addGameToState(game)
+    setButtonType('delete')
   }
 
   const handleDeleteGame = () => {
     //delete game from profile
     profileService.deleteGame(profile._id, game)
+    setButtonType('add')
   }
+
 
   return (  
     <div className="card" style={{width: '15rem'}}>
@@ -19,8 +32,12 @@ const GameCard = ({game, profile, addGameToState}) => {
         <h5 className="card-title">{game.name}</h5>
         {/* <p className="card-text">{game.description_preview}</p> */}
         <button className="btn btn-primary mx-2" data-toggle="collapse" data-target="#collapseExample">Details</button>
-        <button onClick={handleAddGame} className="btn btn-primary">Add Game</button>
-        <button onClick={handleDeleteGame} className="btn btn-primary mx-2">Delete</button>
+        {buttonType === 'delete'
+          ?
+            <button onClick={handleDeleteGame} className="btn btn-primary mx-2">Delete</button>
+          :
+            <button onClick={handleAddGame} className="btn btn-primary">Add</button>
+        }
       </div>
     </div>
   );
