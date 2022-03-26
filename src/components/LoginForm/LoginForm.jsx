1a9fe3bee901bd3b8215b0c6fc84788823c79856
Erin ODea <odea.erin@gmail.com>
2022-03-26 09:20:49 -0500

    just changed login to redirect to profile view

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -21,7 +21,7 @@ const LoginForm = props => {
     try {
       await authService.login(formData)
       props.handleSignupOrLogin()
-      navigate('/')
+      navigate('/myProfile')
     } catch (err) {
       setMessage(err.message)
     }
