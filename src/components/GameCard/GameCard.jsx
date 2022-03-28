8f59529b4bf23ab96837a6c3a355c477a9c04193
Abygaelle <abygaelleb02@gmail.com>
2022-03-28 07:13:51 -0400

    show game details

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -12,12 +12,24 @@ const GameCard = ({game, user}) => {
   }
 
   return (  
-    <div className="card" style={{width: '15rem'}}>
+    <div className="card mx-auto mt-5" style={{width: '18rem'}}>
       <img style={{height: '200px'}} src={game.thumb_url} className="img-thumbnail" alt="..."/>
       <div className="card-body">
         <h5 className="card-title">{game.name}</h5>
         {/* <p className="card-text">{game.description_preview}</p> */}
-        <button className="btn btn-primary mx-2" data-toggle="collapse" data-target="#collapseExample">Details</button>
+        <button className="btn btn-primary" type='button'data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Details</button>
+        <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+        <div class="offcanvas-header">
+        <h5 id="offcanvasRightLabel">Details</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button> 
+        </div>
+        <div class="offcanvas-body">
+          <img style={{width: '350px'}} src={game.thumb_url}/>
+          <strong>Name:</strong> {game.name} <br></br>
+          <strong>Description:</strong> {game.description_preview}
+
+        </div>
+        </div>
         <button onClick={handleAddGame} className="btn btn-primary">Add Game</button>
         <button onClick={handleDeleteGame} className="btn btn-primary mx-2">Delete</button>
       </div>
