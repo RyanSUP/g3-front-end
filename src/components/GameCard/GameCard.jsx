3fff63af580c69aceb1a78cac7be914eb6002050
RMo <morici23@gmail.com>
2022-03-28 15:52:08 -0400

    buttons swap and profile is updated.

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -1,17 +1,29 @@
+import { useState, useEffect } from 'react'
 import * as profileService from '../../services/profileService'
-const GameCard = ({game, user, updateProfile}) => {
+
+const GameCard = ({game, profile, addGameToState}) => {
+  const [buttonType, setButtonType] = useState()
+
+  useEffect(()=> {
+    if(profile.games?.find(profileGame => profileGame._id === game._id)) {
+      setButtonType('delete')
+    } else {
+      setButtonType('add')
+    }
+  }, [profile])
 
   const handleAddGame = () => {
     // add game to profile
-    updateProfile()
-    profileService.addGame(user.profile, game)
+    profileService.addGame(profile._id, game)
+    addGameToState(game)
+    setButtonType('delete')
   }
 
   const handleDeleteGame = () => {
     //delete game from profile
-    profileService.deleteGame(user.profile, game)
+    profileService.deleteGame(profile._id, game)
+    setButtonType('add')
   }
-
   return (  
     <div className="card mx-auto mt-5" style={{width: '18rem'}}>
       <img style={{height: '200px'}} src={game.thumb_url} className="img-thumbnail" alt="..."/>
@@ -20,21 +32,30 @@ const GameCard = ({game, user, updateProfile}) => {
         {/* <p className="card-text">{game.description_preview}</p> */}
         
         <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-        <div className="offcanvas-header">
-        <h5 id="offcanvasRightLabel" className='text-center fs-2' style={{size:'50px'}}><strong>Details</strong></h5>
+          <div className="offcanvas-header">
+            <h5 id="offcanvasRightLabel" className='text-center fs-2' style={{size:'50px'}}><strong>Details</strong></h5>
         
-        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button> 
+            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button> 
+          </div>
+          <div className="offcanvas-body">
+            <img style={{width: '350px'}} src={game.thumb_url}/>
+            <strong>Name:</strong> {game.name} <br></br>
+            <strong>Description:</strong> {game.description_preview}<br></br>
+          </div>
+          {buttonType === 'delete'
+          ?
+            <button onClick={handleDeleteGame} className="btn btn-primary mx-2">Delete</button>
+          :
+            <button onClick={handleAddGame} className="btn btn-primary">Add</button>
+          }
         </div>
-        <div className="offcanvas-body">
-          <img style={{width: '350px'}} src={game.thumb_url}/>
-          <strong>Name:</strong> {game.name} <br></br>
-          <strong>Description:</strong> {game.description_preview}<br></br>
-          
-        </div>
-        <button onClick={handleAddGame} className="btn btn-primary add-btn  ">Add Game</button>
-        </div>
-        <button className="btn btn-primary mx-4" type='button'data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Details</button>
-        <button onClick={handleDeleteGame} className="btn btn-primary mx-auto">Delete</button>
+          {buttonType === 'delete'
+          ?
+            <button onClick={handleDeleteGame} className="btn btn-primary mx-2">Delete</button>
+          :
+            <button onClick={handleAddGame} className="btn btn-primary">Add</button>
+          }
+          <button className="btn btn-primary mx-4" type='button'data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Details</button>
       </div>
     </div>
   );
