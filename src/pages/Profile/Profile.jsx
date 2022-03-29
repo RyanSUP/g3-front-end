60615bb8b58c34983c9db306a21db7499a2a8f21
Abygaelle <abygaelleb02@gmail.com>
2022-03-29 10:58:15 -0400

    fix card scroll

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -2,8 +2,9 @@ import GameList from "../../components/GameList/GameList";
 import AddGroup from "../../components/AddGroup/AddGroup";
 import GroupList from "../../components/GroupList/GroupList";
 import GatheringList from "../../components/GatheringList/GatheringList";
-import { useState, useEffect } from 'react'
+import { useState, useEffect, React } from 'react'
 import { getProfile } from '../../services/profileService';
+import ScrollToTop from "react-scroll-to-top";
 
 const Profile = ({user, handleAddGroup}) => {
   const [profileDetails, setProfileDetails] = useState({})
@@ -28,7 +29,14 @@ const Profile = ({user, handleAddGroup}) => {
         </div>
       )}
       {/* Get all of the profiles games */}
+      <div className='card'>
+        <div className='col scroll'>
+          
       <GameList user={user} games={profileDetails.games} />
+
+      </div>
+
+      </div>
     </>
   );
 }
