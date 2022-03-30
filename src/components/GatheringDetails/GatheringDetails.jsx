99288fb39f2121a3e6b17ad3161b4cd29215b717
RMo <morici23@gmail.com>
2022-03-30 19:00:35 -0400

    Close addGatehring form when submitting

--- src/components/GatheringDetails/GatheringDetails.jsx
+++ src/components/GatheringDetails/GatheringDetails.jsx
@@ -1,30 +1,12 @@
-import {useState, useEffect} from 'react'
 const GatheringDetails = ({gathering}) => {
-  const [details, setDetails] = useState(gathering)
-  const [toggleKey, setToggleKey] = useState(0)
-  const detailComponent =(
-  <>
-    <h4>{gathering.name}</h4>
-    <p>{gathering.location}</p>
-    <p>{gathering.date}</p>
-  </>)
-
-  useEffect(()=>{
-    setToggleKey(toggleKey + 1)
-  },[details])
 
   return (
     <>
-      {detailComponent}
+      <h4>{gathering.name}</h4>
+      <p>{gathering.location}</p>
+      <p>{gathering.date}</p>
     </>
   );
 }
  
 export default GatheringDetails;
-
-// <ToggleForm 
-// key={idx} 
-// buttonText={'edit'} 
-// altComponent={<GatheringDetails gathering={gathering} />}
-// form={<UpdateGathering group={group} gathering={gathering} />} 
-// />
--- src/components/GatheringList/GatheringList.jsx
+++ src/components/GatheringList/GatheringList.jsx
@@ -1,13 +1,41 @@
-import ToggleForm from '../ToggleForm/ToggleForm';
 import GatheringDetails from '../GatheringDetails/GatheringDetails'
 import UpdateGathering from '../UpdateGathering/UpdateGathering'
-const GatheringList = ({group, gatherings }) => {
+import ToggleForm from '../ToggleForm/ToggleForm';
+import { useState, useEffect } from 'react'
+import AddGathering from '../AddGathering/AddGathering';
+import * as groupService from '../../services/groupService'
+import * as gatheringService from '../../services/gatheringService'
 
+const GatheringList = ({group, gatherings, profile}) => {
+  const [leGatherings, setLeGatherings] = useState(profile.groups)
+  const [toggleKey, setToggleKey] = useState(0)
+
+
+  useEffect(()=>{
+    if(gatherings) { setLeGatherings(gatherings) }
+  }, [gatherings])
+
+  useEffect(()=> {
+    setToggleKey(toggleKey + 1)
+  }, [leGatherings])
+  // Keep all gatherings in state
+  // gathering details is passed a function to update
+
+  const handleAddGathering = newGatheringData => {
+    groupService.addGathering(group._id, newGatheringData)
+    .then(newGathering => setLeGatherings([...leGatherings, newGathering]))
+  }
 
   return (
     <>
-      {gatherings?.map((gathering, idx) => 
-          <GatheringDetails gathering={gathering} />
+      <ToggleForm key={toggleKey} form={<AddGathering handleAddGathering={handleAddGathering} />} buttonText={'New gathering'} />
+      {leGatherings?.map((gathering, idx) => 
+         <ToggleForm 
+            key={idx} 
+            buttonText={'edit'} 
+            altComponent={<GatheringDetails gathering={gathering} />}
+            form={<UpdateGathering group={group} gathering={gathering} />} 
+          />
       )}
     </>
   );
