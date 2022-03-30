25e4224fa08dc4a37ff69c0b3fd0ded2b9d12fd3
RMo <morici23@gmail.com>
2022-03-30 13:40:53 -0400

    Fix button overlap

--- src/components/GroupCard/GroupCard.jsx
+++ src/components/GroupCard/GroupCard.jsx
@@ -5,19 +5,17 @@ const GroupCard = ({profile, group, handleDeleteGroup, handleLeaveGroup}) => {
 
   return ( 
     <div className="card">
-      <div className='row row-cols-1 row-cols-md-2 g-4'> 
-          <div>
-            <Link  to={`/groups/${group._id}`} state={{group}}>
-              <img style={{width: "250px"}}src={group.image} alt="group pic" />
-              <h3>{group.name}</h3>
-            </Link>
-            {group.manager === profile._id
-              ?
-                dltBtn
-              :
-                leaveBtn
-            }
-          </div>
+      <div> 
+        <Link  to={`/groups/${group._id}`} state={{group}}>
+          <img style={{width: "250px"}}src={group.image} alt="group pic" />
+          <h3>{group.name}</h3>
+        </Link>
+        {group.manager === profile._id
+          ?
+            dltBtn
+          :
+            leaveBtn
+        }
       </div>
     </div>
   );
