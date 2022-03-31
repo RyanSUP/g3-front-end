09be592eccbcaef8aa2183f0107753229c8d33c8
Abygaelle <abygaelleb02@gmail.com>
2022-03-31 10:13:10 -0400

    fix game card

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -4,10 +4,8 @@ import * as profileService from '../../services/profileService'
 
 const GameCard = ({game, profile, addGameToState, updateOffCanvas}) => {
   const [currentButton, setCurrentButton] = useState()
-
   const addButton = <button onClick={handleAddGame} className="btn flex-grow-1 btn-success">Add</button>
   const dltButton = <button onClick={handleDeleteGame} className="btn flex-grow-1 btn-danger">Delete</button>
-
 
   useEffect(()=> {
     if(profile.games?.find(profileGame => profileGame._id === game._id)) {
@@ -33,27 +31,28 @@ const GameCard = ({game, profile, addGameToState, updateOffCanvas}) => {
   }
 
   return (  
-
     <div className="card shadow m-4" style={{width: '18rem'}}>
-      <div className="card-header text-center">
-        <p className="fs-4 m-0">{game.name}</p>        
-      </div>
-      <div className='card-body d-flex flex-row justify-content-between align-items-center m-0'>
-          <span className='fs-5'>{game.players} <i className="fa-solid fa-people-group"></i></span>
-          <span className='fs-5'>{game.playtime} <i className="fa-solid fa-clock"></i></span>
-      </div>
+    <div className="card-header d-flex flex-row justify-content-between align-items-center m-0">
+        <span className='fs-5'>{game.playtime} <i className="fa-solid fa-clock"></i></span>
+        <span className='fs-5'>{game.players} <i className="fa-solid fa-people-group"></i></span>
+        
+        <button className="info-btn btn btn-outline-primary p=0" type='button'data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" onClick={()=>updateOffCanvas(game, currentButton)}><i className="fa-solid fa-circle-info"></i></button>
+    </div>
+    <div className='card-body text-center'>
+      <p className="fs-4 m-0">{game.name}</p>        
+    </div>
+    <img style={{height: '200px'}} src={game.thumb_url} className="img-thumbnail" alt="..."/>
+    <div className="card-footer text-muted">
+      <div className="d-flex flex-column">
+        {currentButton}
 
-      <img style={{height: '200px'}} src={game.thumb_url} className="img-thumbnail" alt="..."/>
-      <div className="card-footer text-muted">
-        <div className="d-flex flex-column">
-          {currentButton}
-          <button className="btn btn-primary p=0" type='button'data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" onClick={()=>updateOffCanvas(game, currentButton)}>Details <i className="fa-solid fa-circle-info"></i></button>
-        </div>
       </div>
     </div>
+  </div>
   );
 }
 //  type='button'data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" onClick={()=>updateOffCanvas(game, currentButton)}
 
 
 export default GameCard;
+
--- src/components/GroupCard/GroupCard.jsx
+++ src/components/GroupCard/GroupCard.jsx
@@ -1,44 +1,25 @@
 import { Link } from "react-router-dom"
-import ToggleBtn from "../GroupCardBtn/GroupCardBtn"
-const GroupCard = ({group}) => {
+const GroupCard = ({profile, group, handleDeleteGroup, handleLeaveGroup}) => {
+  const leaveBtn = <button className="btn btn-outline-danger" type="submit" onClick={() => handleLeaveGroup(group._id)}>Leave</button>
+  const dltBtn = <button className="btn btn-outline-danger" type="submit" onClick={() => handleDeleteGroup(group._id)}>Disband</button>
 
   return ( 
-  <> 
+
     <div className="card">
-      <div>
-        
+      <div> 
         <Link  to={`/groups/${group._id}`} state={{group}}>
           <img style={{width: "250px"}}src={group.image} alt="group pic" />
           <h3>{group.name}</h3>
         </Link>
-        <ToggleBtn/>
-=======
-
+        {group.manager === profile._id
+          ?
+            dltBtn
+          :
+            leaveBtn
+        }
       </div>
     </div>
-    </>
   );
 }
-
-
-export default GroupCard;
-
  
-
-
-    // {/* <div className="card">
-    //   <div> 
-    //     <Link  to={`/groups/${group._id}`} state={{group}}>
-    //       <img style={{width: "250px"}}src={group.image} alt="group pic" />
-    //       <h3>{group.name}</h3>
-    //     </Link>
-    //     {group.manager === profile._id
-    //       ?
-    //         dltBtn
-    //       :
-    //         leaveBtn
-    //     }
-    //   </div>
-      
-    // </div> */}
-
+export default GroupCard;
