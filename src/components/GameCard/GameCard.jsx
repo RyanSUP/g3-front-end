3ef505937077f8a4cf2a701a855939c6866acf79
RMo <morici23@gmail.com>
2022-03-31 00:19:09 -0400

    Card css updates

--- src/components/GameCard/GameCard.jsx
+++ src/components/GameCard/GameCard.jsx
@@ -31,19 +31,18 @@ const GameCard = ({game, profile, addGameToState, updateOffCanvas}) => {
 
   return (  
     <div className="card shadow m-4" style={{width: '18rem'}}>
-      <div className="card-header d-flex flex-row justify-content-between align-items-center m-0">
+      <div className="card-header text-center">
+        <p className="fs-4 m-0">{game.name}</p>        
+      </div>
+      <div className='card-body d-flex flex-row justify-content-between align-items-center m-0'>
           <span className='fs-5'>{game.players} <i className="fa-solid fa-people-group"></i></span>
           <span className='fs-5'>{game.playtime} <i className="fa-solid fa-clock"></i></span>
-          <button className="btn btn-outline-primary fs-4" type='button'data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" onClick={()=>updateOffCanvas(game, currentButton)}><i className="fa-solid fa-circle-info"></i></button>
-      </div>
-      <div className='card-body'>
-        <p className="fs-4 m-0">{game.name}</p>        
       </div>
       <img style={{height: '200px'}} src={game.thumb_url} className="img-thumbnail" alt="..."/>
       <div className="card-footer text-muted">
-        <div className="d-flex">
+        <div className="d-flex flex-column">
           {currentButton}
-
+          <button className="btn btn-primary p=0" type='button'data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" onClick={()=>updateOffCanvas(game, currentButton)}>Details <i className="fa-solid fa-circle-info"></i></button>
         </div>
       </div>
     </div>
